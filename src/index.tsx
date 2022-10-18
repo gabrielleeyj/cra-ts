de6ba155583be67f17da36ef84c978b06ba6f5d3
Gabriel Lee <gabriel.lee@transcelestial.com>
2022-10-18 11:34:37 +0800

    fix: clean up defaults with working project

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path='./index.d.ts'/>
+import * as serviceWorker from './serviceWorker';
+import configureStore from './configureStore';
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+import Main from './main';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// @ts-ignore
+const store = configureStore(history);
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <Main store={store} />
+  </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.unregister();
